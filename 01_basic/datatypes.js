a1b3f667e28cda60885b5d0454d7b768f54b2659
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-05-13 17:21:00 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat al js code as newer version
+
+//alert("Hello World!")  we are using node.js not browser
+
+let value = null
+let state = undefined
+
+
+console.log(typeof undefined)
