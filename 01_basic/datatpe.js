f3a4652a97f84945a3c9de25eb53a51594fa7e0d
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-05-14 14:39:10 +0000

    datatpe

---
+++ 01_basic/datatpe.js
@@ -0,0 +1,29 @@
+// Primitive 
+
+// Primitive data types are the most basic data types in JavaScript. They are immutable and represent a single value.
+
+// The 7 primitive data types in JavaScript are:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+// Reference data types are more complex data types. They are mutable and can hold multiple values. They are also known as non-primitive data types.
+
+// The 3 reference data types in JavaScript are:
+// 1. Object
+// 2. Array
+// 3. Function 
+
+const score = 100; 
+const scoreValue = 100.32
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = symbol('123')
+const anotherId = symbol('123')
