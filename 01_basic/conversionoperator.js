3f3a1f5af853f7127d6992f8619d5dee411c54b4
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-05-14 12:09:41 +0000

    conversionoperator

--- 01_basic/conversionoperator.js
+++ 01_basic/conversionoperator.js
@@ -1,10 +1,10 @@
 let score = undefined
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueNumber = Number(score)
-console.log(typeof valueNumber)
+//console.log(typeof valueNumber)
 
-console.log(valueNumber) // NaN : not a number
+//console.log(valueNumber) // NaN : not a number
 
 // "33" => 33
 // "33.3" => 33.3
@@ -19,7 +19,7 @@ let isLoggedIn = "Gaurav"
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) // true
+//console.log(booleanIsLoggedIn) // true
 
 // 1 => true
 // 0 => false
@@ -30,4 +30,23 @@ console.log(booleanIsLoggedIn) // true
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
+//console.log(stringNumber)
+
+// ************ operations ************
+
+let value =3
+let negValue = -value
+
+// console.log(negValue)
+// console.log(2%5)
+
+// console.log(2 + "5")
+// console.log("5" +2 +2) // 522
+// console.log(2 + 2 + "5") // 45
+// console.log(2 - "5") // 2-5 = -3
+
+console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
