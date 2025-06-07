9b4a44efd9489ab7e8ab69ce1af4a0866b8eb245
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-06-07 19:00:05 +0000

    commit loops

--- 01_basic/loops.js
+++ 01_basic/loops.js
@@ -1,6 +1,29 @@
-let ans =0;
+// let ans =0;
 
-for(let i =1; i<=50; i++){
-    ans = ans + i;
-}
-console.log(ans);
+// for(let i =1; i<=50; i++){
+//     ans = ans + i;
+// }
+// console.log(ans);
+
+//-------------Date and Time-------------------
+
+// let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.getTime())
+
+//console.log(typeof mydate)
+
+let mytmestamp = Date.now()
+let newdate = new Date(mytmestamp)
+console.log(mytmestamp)
+console.log(Math.floor(mytmestamp/1000))
+console.log(newdate.toLocaleDateString())
+
+let k = newdate.toLocaleString('default',{
+    weekday: 'long',
+})
+console.log(k)
+
