01e54bb56ca2c50f76bdbfb6bdfb161117897752
Gaurav Kumar <150705514+krgaurav7@users.noreply.github.com>
2025-06-08 06:09:00 +0000

    commit scope

--- 01_basic/scope.js
+++ 01_basic/scope.js
@@ -23,4 +23,55 @@ function one (){
     two()
 }
 
-one ()
+//one ()
+
+
+//
+// ------------- Arrow Functions ------------- and This functions
+const user = {
+    username : "Gaurav",
+    price : 999,
+
+    welcomeMessage :function(){
+        console.log(`${this.username} is welcome to the course at price ${this.price}`)
+       // console.log(this)
+    }
+}
+user.username = "Gaurav Singh"
+//console.log(user)
+//user.welcomeMessage()
+//console.log(this) // this refers to the global object in non-strict mode, undefined in strict mode
+
+function one1 (){
+    console.log(this) // this refers to the global object in non-strict mode, undefined in strict mode
+}
+
+//one1()
+
+const chai = function(){
+    let username = "Gaurav Singh"
+    console.log(this.username)
+}
+
+// chai()
+
+
+// Arrow function
+
+const chai1 = () =>{
+    let username = "Gaurav Singh"
+    console.log(this)
+}
+//chai1()
+
+//const addTwo = (a,b) =>{
+  //  return a+b
+//}
+
+
+const addTwo = (a,b) => ( a+b ) // implicit return
+
+const addThree = (a,b,c) => ({username : "Gaurav Singh"}) // explicit return
+
+console.log(addThree(4,6,5))
+console.log(addTwo(4,6))
